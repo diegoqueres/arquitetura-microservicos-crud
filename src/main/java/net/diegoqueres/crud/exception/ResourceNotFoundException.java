e18b534ea48d0bf259d8da5b5e1e179cb2f178b2
Diego Queres <diego.queres@stoom.com.br>
2021-05-22 12:50:42 -0300

    Criando o Microserviço CRUD - ProdutoService

---
+++ src/main/java/net/diegoqueres/crud/exception/ResourceNotFoundException.java
@@ -0,0 +1,13 @@
+package net.diegoqueres.crud.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class ResourceNotFoundException extends RuntimeException {
+    private static final long serialVersionUID = 5337840272763952219L;
+
+    public ResourceNotFoundException(String exception) {
+        super(exception);
+    }
+}
--- src/main/java/net/diegoqueres/crud/services/ProdutoService.java
+++ src/main/java/net/diegoqueres/crud/services/ProdutoService.java
@@ -1,9 +1,15 @@
 package net.diegoqueres.crud.services;
 
 import net.diegoqueres.crud.data.dto.ProdutoDTO;
+import net.diegoqueres.crud.entity.Produto;
+import net.diegoqueres.crud.exception.ResourceNotFoundException;
 import net.diegoqueres.crud.repository.ProdutoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class ProdutoService {
@@ -16,7 +22,38 @@ public class ProdutoService {
     }
 
     public ProdutoDTO create(ProdutoDTO produtoDTO) {
-        return null;
+        Produto produto = produtoRepository.save(Produto.create(produtoDTO));
+        return ProdutoDTO.create(produto);
     }
 
+    public Page<ProdutoDTO> findAll(Pageable pageable) {
+        var page = produtoRepository.findAll(pageable);
+        return page.map(this::convertToProdutoDTO);
+    }
+
+    private ProdutoDTO convertToProdutoDTO(Produto produto) {
+        return ProdutoDTO.create(produto);
+    }
+
+    public ProdutoDTO findById(Long id) {
+        var entity = produtoRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("No records found for this ID"));
+        return ProdutoDTO.create(entity);
+    }
+
+    public ProdutoDTO update(ProdutoDTO produtoDTO) {
+        final Optional<Produto> optionalProduto = produtoRepository.findById(produtoDTO.getId());
+        if (!optionalProduto.isPresent())
+            new ResourceNotFoundException("No records found for this ID");
+
+        Produto produto = Produto.create(produtoDTO);
+        produto = produtoRepository.save(produto);
+        return ProdutoDTO.create(produto);
+    }
+
+    public void delete(Long id) {
+        var entity = produtoRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("No records found for this ID"));
+        produtoRepository.delete(entity);
+    }
 }
