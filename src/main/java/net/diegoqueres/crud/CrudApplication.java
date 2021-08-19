63f060eb11186bb42868ae39b8a86286cef55b9a
Diego Queres <diegoqueres81@gmail.com>
2021-08-18 21:31:43 -0300

    Criado o projeto Netflix Eureka - Parte 1

--- src/main/java/net/diegoqueres/crud/CrudApplication.java
+++ src/main/java/net/diegoqueres/crud/CrudApplication.java
@@ -5,10 +5,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @EnableJpaRepositories("net.diegoqueres.crud.repository")
 public class CrudApplication {
 	public static void main(String[] args) {
